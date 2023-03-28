11998580f93bb1734e49faabe3912db3c6d96851
nathan-obace <nathanobace@gmail.com>
2023-03-28 16:04:47 +0300

    Fix baseUrl

--- cypress/e2e/home_page.cy.js
+++ cypress/e2e/home_page.cy.js
@@ -1,6 +1,6 @@
 describe("The Home Page", () => {
   it("successfully loads", () => {
-    cy.visit("");
+    cy.visit("/");
     cy.get('button[aria-label="newsletter_popup_close-cta"]').click();
   });
 });
