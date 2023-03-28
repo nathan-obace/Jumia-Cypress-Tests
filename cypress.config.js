65a0a50266a7b8f8c8edc1fe36517caa85631853
nathan-obace <nathanobace@gmail.com>
2023-03-28 15:56:37 +0300

    add yml file

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "be2sbv",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: "https://www.jumia.ug",
   },
 });
--- cypress/e2e/home_page.cy.js
+++ cypress/e2e/home_page.cy.js
@@ -1,6 +1,6 @@
 describe("The Home Page", () => {
   it("successfully loads", () => {
-    cy.visit("https://www.jumia.ug/");
+    cy.visit("");
     cy.get('button[aria-label="newsletter_popup_close-cta"]').click();
   });
 });
